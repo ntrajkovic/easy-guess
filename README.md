fac98f0238e5f7da699cfe9707aa1e0907d609da
Nenad Trajkovic <nenad.trajkovic@gmail.com>
2022-01-12 10:56:11 +0100

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# easy-guess
+# Easy Guess
 Pseudo random number generator
+
+## License
+This project is licensed under [MIT License](LICENSE "Read the LICENSE file").
+
+[Know your rights](https://choosealicense.com/licenses/mit/ "Read about MIT License permissions").
+
+&copy; 2022 Nenad Trajkovic
---
+++ easyguess.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# ********************************************************************
+# easyguess.py
+# Pseudo random number generator
+# (C) 2022 Nenad Trajkovic, MIT License
+# ********************************************************************
+
+def easy_guess():
+    pass
+
+if __name__ == "__main__":
+    pass
